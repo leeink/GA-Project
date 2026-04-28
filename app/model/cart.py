dd05056e555899f24d380c6389b6aa547c768deb
leeink <zzxc78@naver.com>
2026-04-28 14:22:37 +0900

    Modify Column

--- app/model/cart.py
+++ app/model/cart.py
@@ -12,7 +12,7 @@ class Cart(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     product_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("product.id"))
-    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("siteuser.id"))
     cart_quantity: Mapped[int] = mapped_column()
 
     product: Mapped["Product"] = relationship(back_populates="carts")
--- app/model/sales_record.py
+++ app/model/sales_record.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -12,9 +12,9 @@ class SalesRecord(Base):
     __tablename__ = "sales_record"
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    sold_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    sold_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
     product_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("product.id"))
-    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("siteuser.id"))
     quantity: Mapped[int] = mapped_column()
     sales_price: Mapped[int] = mapped_column()
 
